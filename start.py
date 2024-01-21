5107935971073ecb6f884bc7390b7f4f02f8829b
Aman-SS <amanshankar.singh@gmail.com>
2024-01-21 15:19:40 +0530

    first python university commit

---
+++ start.py
@@ -0,0 +1,8 @@
+# python university
+class University:
+    def __init__(self, name, started):
+        self.name = name
+        self.started = started
+
+U1 = University("Python University", "21-01-2024");
+print(U1.name)
