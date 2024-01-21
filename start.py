0595504e2c84cadd31a45d27fd7e790891259a6e
Aman-SS <amanshankar.singh@gmail.com>
2024-01-21 17:03:17 +0530

    define courses and mentors class

--- start.py
+++ start.py
@@ -5,4 +5,28 @@ class University:
         self.started = started
 
 U1 = University("Python University", "21-01-2024");
-print(U1.name)
+print("=> University Name: "+U1.name)
+print("=> Starting Date: "+U1.started,)
+print(end="\n")
+
+# university courses
+class CoursesList:
+    def __init__(self, courses):
+        self.courses = courses
+
+CL = CoursesList(courses=["1. Python Programming", "2. Data Structure and Algorithm in Python"])
+print("=> Courses")
+for course in CL.courses:
+    print(course)
+print(end="\n")
+
+# mentors
+class MentorsList:
+    def __init__(self, mentors):
+        self.mentors = mentors
+
+ML = MentorsList(mentors=["1. Aman Shankar Singh", "2. Dev Aman"])
+print("=> Mentors")
+for mentor in ML.mentors:
+    print(mentor)
+print(end="\n")
